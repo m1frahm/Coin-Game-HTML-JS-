b1fa56a91d8fb5b48f03b55f77afd91433b33ef9
Mifrah Malik <mifrah.malik@gmail.com>
2023-02-07 16:11:02 -0500

    lay out of game vision complete

---
+++ index.js
@@ -0,0 +1,10 @@
+//Psuedocode
+
+//Need flip function with two parameters that is heads, tails
+
+// coin game in a class
+// one of the methods could be flip
+// one of the methods could be reset
+//this.Heads
+//this.Tails
+//this.Score and set the max 5
