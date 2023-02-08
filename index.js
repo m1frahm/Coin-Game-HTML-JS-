66259a2a613846ad1c0c3c90f3622898d30c046f
Mifrah Malik <mifrah.malik@gmail.com>
2023-02-08 14:13:04 -0500

    added onClick to button function to be created in JS

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 //Psuedocode
 
-//Need flip function with two parameters that is heads, tails
+//Do we need flip function with two parameters that is heads, tails
 
-// coin game in a class
-// one of the methods could be flip
-// one of the methods could be reset
-//this.Heads
-//this.Tails
+
+//Create a CoinGame class that has 2 properties
+// this.
 //this.Score and set the max 5
+
+//Maybe create a method called Win
+
+class CoinGame {
+    constructor(coinName, Score) {
+        this.coinName = ["Heads","Tails"]
+        this.score = 0
+    } 
+    flipCoin () {
+        // this will either show heads image or tail image using random?
+    }
+    showScore() {
+        //you would probably update an HTML element that shows the score
+        console.log(`Your current score is: ${this.score}`);
+    }
+    resetScore () {
+        //reset score back to 0
+    }
+    addPoint() {
+        // this would be called when the person clicks on the correct coin 
+        this.score+=1;
+      }
+    }
+
+//we need to pass values into our objects
+let myGame = new CoinGame(["Heads"])
