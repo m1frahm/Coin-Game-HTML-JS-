677338509fe78561421e924ad57f26aa46c6d3f6
Mifrah Malik <mifrah.malik@gmail.com>
2023-02-08 15:35:44 -0500

    flipCoin function in JS works

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 //Psuedocode
+// 
 
-//Do we need flip function with two parameters that is heads, tails
 
-
-//Create a CoinGame class that has 2 properties
-// this.
-//this.Score and set the max 5
-
-//Maybe create a method called Win
-
-class CoinGame {
-    constructor(coinName, Score) {
-        this.coinName = ["Heads","Tails"]
-        this.score = 0
-    } 
-    flipCoin () {
-        // this will either show heads image or tail image using random?
-    }
-    showScore() {
-        //you would probably update an HTML element that shows the score
-        console.log(`Your current score is: ${this.score}`);
-    }
-    resetScore () {
-        //reset score back to 0
-    }
-    addPoint() {
-        // this would be called when the person clicks on the correct coin 
-        this.score+=1;
-      }
+// This is the flipCoin function in JS
+function flipCoin () {
+        // assign random value where 0 = heads & 1 = tails
+        var flip = Math.floor(Math.random() * 2)
+        console.log(flip);
+        if (flip === 0)
+        {
+            drawHeads();
+        } else {
+            drawTails();
+        }
     }
 
-//we need to pass values into our objects
-let myGame = new CoinGame(["Heads"])
+    // This is the flipCoin function in JS
+    function drawHeads() {
+        console.log("headimage");
+    }
+
+    //
+
+    function drawTails() {
+        console.log("tailimage");
+    }
+
+    function printMessage() {
+        //print winning message 
+    }
+
+   
+
+
